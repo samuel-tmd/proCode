a63199e8620f6292a0268386adb829bf3039fa90
Samuel Dias <samuel_tmdias@hotmail.com>
2023-11-15 18:07:40 -0300

    Adição de try catches

--- src/main/java/com/procode/pmt/controller/TaskController.java
+++ src/main/java/com/procode/pmt/controller/TaskController.java
@@ -35,85 +35,117 @@ public class TaskController
     public ResponseEntity listAll()
     {
         Map returnData = new HashMap<String, Object>();
-        returnData.put("data", taskRepository.findAll());
+        try 
+        {
+            returnData.put("data", taskRepository.findAll());
+        } 
+        catch (Exception e) 
+        {
+            returnData.put("message", e.getMessage());
+            return new ResponseEntity(returnData, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return new ResponseEntity(returnData, HttpStatus.OK);
     }
     @PostMapping
     public ResponseEntity create(@RequestBody Task newTask)
     {
         Map returnData = new HashMap<String, Object>();
-        if(!hasProjectId(newTask))
+        try 
         {
-            returnData.put("message", "ProjectId cannot be null");
-            return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
-        }
-        Optional<Project> projectOptional = projectRepository.findById(newTask.getProjectId());
-        if(!projectOptional.isPresent())
+            if(!hasProjectId(newTask))
+            {
+                returnData.put("message", "ProjectId cannot be null");
+                return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
+            }
+            Optional<Project> projectOptional = projectRepository.findById(newTask.getProjectId());
+            if(!projectOptional.isPresent())
+            {
+                returnData.put("message", "Project not found");
+                return new ResponseEntity(returnData, HttpStatus.NOT_FOUND);
+            }
+            Project parentProject = projectOptional.get();
+            newTask.setParentProject(parentProject);
+            newTask = taskRepository.save(newTask);
+            parentProject.addTask(newTask);
+            projectRepository.save(parentProject);
+            List<Task> tasks = new ArrayList<Task>();
+            tasks.add(newTask);
+            returnData.put("data", tasks);
+        } 
+        catch (Exception e) 
         {
-            returnData.put("message", "Project not found");
-            return new ResponseEntity(returnData, HttpStatus.NOT_FOUND);
+            returnData.put("message", e.getMessage());
+            return new ResponseEntity(returnData, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        Project parentProject = projectOptional.get();
-        newTask.setParentProject(parentProject);
-        newTask = taskRepository.save(newTask);
-        parentProject.addTask(newTask);
-        projectRepository.save(parentProject);
-        List<Task> tasks = new ArrayList<Task>();
-        tasks.add(newTask);
-        returnData.put("data", tasks);
         return new ResponseEntity(returnData, HttpStatus.CREATED);
     }
     @PatchMapping
     public ResponseEntity update(@RequestBody Task taskToUpdate) 
     {
         Map returnData = new HashMap<String, Object>();
-        if(!hasId(taskToUpdate))
+        try 
         {
-            returnData.put("message", "Id cannot be null");
-            return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
-        }
-        Optional<Task> taskOptional = taskRepository.findById(taskToUpdate.getId());
-        if(!taskOptional.isPresent())
+            if(!hasId(taskToUpdate))
+            {
+                returnData.put("message", "Id cannot be null");
+                return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
+            }
+            Optional<Task> taskOptional = taskRepository.findById(taskToUpdate.getId());
+            if(!taskOptional.isPresent())
+            {
+                returnData.put("message", "Task not found");
+                return new ResponseEntity(returnData, HttpStatus.NOT_FOUND);
+            }
+            Task existingTask = taskOptional.get();
+            if(!hasProjectId(taskToUpdate))
+            {
+                returnData.put("message", "ProjectId cannot be null");
+                return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
+            }
+            if(existingTask.getProjectId() != taskToUpdate.getProjectId())
+            {
+                returnData.put("message", "It's not possible to change the Task's Project.");
+                return new ResponseEntity(returnData, HttpStatus.BAD_REQUEST);
+            }
+            taskToUpdate = taskRepository.save(taskToUpdate);
+            List<Task> tasks = new ArrayList<Task>();
+            tasks.add(taskToUpdate);
+            returnData.put("data", tasks);
+        } 
+        catch (Exception e) 
         {
-            returnData.put("message", "Task not found");
-            return new ResponseEntity(returnData, HttpStatus.NOT_FOUND);
+            returnData.put("message", e.getMessage());
+            return new ResponseEntity(returnData, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        Task existingTask = taskOptional.get();
-        if(!hasProjectId(taskToUpdate))
-        {
-            returnData.put("message", "ProjectId cannot be null");
-            return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
-        }
-        if(existingTask.getProjectId() != taskToUpdate.getProjectId())
-        {
-            returnData.put("message", "It's not possible to change the Task's Project.");
-            return new ResponseEntity(returnData, HttpStatus.BAD_REQUEST);
-        }
-        taskToUpdate = taskRepository.save(taskToUpdate);
-        List<Task> tasks = new ArrayList<Task>();
-        tasks.add(taskToUpdate);
-        returnData.put("data", tasks);
         return new ResponseEntity(returnData, HttpStatus.OK);
     }
     @DeleteMapping
     public ResponseEntity delete(@RequestBody Task taskToDelete) 
     {
         Map returnData = new HashMap<String, Object>();
-        if(!hasId(taskToDelete))
+        try 
         {
-            returnData.put("message", "Id cannot be null");
-            return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
-        }
-        Optional<Project> projectOptional = projectRepository.findById(taskToDelete.getProjectId());
-        if(!projectOptional.isPresent())
+            if(!hasId(taskToDelete))
+            {
+                returnData.put("message", "Id cannot be null");
+                return new ResponseEntity(returnData, HttpStatus.PRECONDITION_FAILED);
+            }
+            Optional<Project> projectOptional = projectRepository.findById(taskToDelete.getProjectId());
+            if(!projectOptional.isPresent())
+            {
+                returnData.put("message", "Project not found");
+                return new ResponseEntity(returnData, HttpStatus.NOT_FOUND);
+            }
+            Project parentProject = projectOptional.get();
+            parentProject.removeTask(taskToDelete);
+            projectRepository.save(parentProject);
+            taskRepository.delete(taskToDelete);
+        } 
+        catch (Exception e) 
         {
-            returnData.put("message", "Project not found");
-            return new ResponseEntity(returnData, HttpStatus.NOT_FOUND);
+            returnData.put("message", e.getMessage());
+            return new ResponseEntity(returnData, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        Project parentProject = projectOptional.get();
-        parentProject.removeTask(taskToDelete);
-        projectRepository.save(parentProject);
-        taskRepository.delete(taskToDelete);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
     public static Boolean hasId(Task taskToVerify)
